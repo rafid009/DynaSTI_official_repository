978bca19537f3a5fc11a1be811b01f2315ed3266
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-09-09 22:00:52 -0700

    PriSTI added for test for all 1-5

--- datasets/dataset_awn.py
+++ datasets/dataset_awn.py
@@ -402,23 +402,27 @@ class AWN_Dataset(Dataset):
             
             B, L, N, K = X.shape
 
-            X_temp = np.zeros((X_.shape[0], X_.shape[1], total_stations, X_.shape[3]))
-            X_temp[:, :, :X_.shape[2], :] = X_
-            X_pristi = X_temp.reshape(B, L, -1)
+            
             
             
             X_ = X.reshape(B, L, -1, n_features)
-            if self.is_separate:
-                X = X.reshape(B, L, -1)
+            X = X.reshape(B, L, -1)
+
+            X_temp = np.zeros((X_.shape[0], X_.shape[1], total_stations, X_.shape[3]))
+            X_temp[:, :, :X_.shape[2], :] = X_
+            X_pristi = X_temp.reshape(B, L, -1)
+
+            # if self.is_separate:
+            
                 
-            else:
+            # else:
                 
-                X_temp = np.zeros((X_.shape[0], X_.shape[1], X_.shape[2] + 1, X_.shape[3]))
-                X_temp[:, :, :X_.shape[2], :] = X_
-                X = X_temp.reshape(B, L, -1)
-                X_loc_ = np.zeros((X_loc.shape[0] + 1, X_loc.shape[1]))
-                X_loc_[:X_loc.shape[0]] = X_loc
-                X_loc = X_loc_
+            #     X_temp = np.zeros((X_.shape[0], X_.shape[1], X_.shape[2] + 1, X_.shape[3]))
+            #     X_temp[:, :, :X_.shape[2], :] = X_
+            #     X = X_temp.reshape(B, L, -1)
+            #     X_loc_ = np.zeros((X_loc.shape[0] + 1, X_loc.shape[1]))
+            #     X_loc_[:X_loc.shape[0]] = X_loc
+            #     X_loc = X_loc_
             
 
             X_loc = X_loc.reshape(-1) # N*3
