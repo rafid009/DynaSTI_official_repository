78785acbcae53c6f2f8203acb7702edeb625981d
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-11-05 10:17:05 -0800

    Bayesian sensor replacement added 1-26

--- datasets/dataset_nasce.py
+++ datasets/dataset_nasce.py
@@ -558,12 +558,12 @@ class NASCE_Dataset(Dataset):
         return len(self.observed_values)
 
 
-def get_dataloader(total_stations, mean_std_file, n_features, batch_size=16, missing_ratio=0.2, is_test=False, type='year', data='temps', simple=False, is_neighbor=False, spatial_choice=None, is_separate=False, is_multi=False, is_pristi=False, southeast=False, sparse=False):
+def get_dataloader(total_stations, mean_std_file, n_features, batch_size=16, missing_ratio=0.2, is_test=False, type='year', data='temps', simple=False, is_neighbor=False, spatial_choice=None, is_separate=False, is_multi=False, is_pristi=False, southeast=False, sparse=False, missing_dims=-1):
     # np.random.seed(seed=seed)
     train_dataset = NASCE_Dataset(total_stations, mean_std_file, n_features, rate=0.0001, simple=simple, is_neighbor=is_neighbor, spatial_choice=spatial_choice, is_separate=is_separate, is_pristi=is_pristi)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-    test_dataset = NASCE_Dataset(total_stations, mean_std_file, n_features, rate=missing_ratio, pattern=None, is_valid=True, spatial=True, simple=simple, is_neighbor=is_neighbor, spatial_choice=spatial_choice, is_separate=is_separate, missing_dims=10 if is_multi else -1, is_pristi=is_pristi, southeast=southeast, sparse=sparse)
+    test_dataset = NASCE_Dataset(total_stations, mean_std_file, n_features, rate=missing_ratio, pattern=None, is_valid=True, spatial=True, simple=simple, is_neighbor=is_neighbor, spatial_choice=spatial_choice, is_separate=is_separate, missing_dims=10 if is_multi else -1, is_pristi=is_pristi, southeast=southeast, sparse=sparse, missing_dims=missing_dims)
     
     if is_test:
         test_loader = DataLoader(test_dataset, batch_size=1)
--- random_sensor_placement_nacse.py
+++ random_sensor_placement_nacse.py
@@ -218,10 +218,12 @@ class NewLocationCoordsAndUncertainty:
     def __repr__(self):
         return f"Coords: {self.coords.numpy()}, Uncertainty: {self.uncertainty}"
 
-train_loader, test_loader = get_dataloader(total_stations, mean_std_file, n_features, batch_size=8, missing_ratio=0.02, type=data_type, data=data, simple=simple, is_neighbor=is_neighbor, spatial_choice=spatial_choice, is_separate=is_separate, is_multi=is_multi, is_test=True, southeast=True, sparse=True)
-
 N = 4
-M = 5  # Number of virtual sensors to evaluate uncertainty on
+M = 5 # Number of virtual sensors to evaluate uncertainty on
+
+train_loader, test_loader = get_dataloader(total_stations, mean_std_file, n_features, batch_size=8, missing_ratio=0.02, type=data_type, data=data, simple=simple, is_neighbor=is_neighbor, spatial_choice=spatial_choice, is_separate=is_separate, is_multi=is_multi, is_test=True, southeast=True, sparse=True, missing_dims=M)
+
+  
 lr = 0.01
 total_points = 50
 iters = 10 #00
