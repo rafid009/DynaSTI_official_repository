60ad8925d34789212ef908ed5ad8eab95c0196ab
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-11-19 11:16:21 -0800

    sensor placement evaluation 1-55

--- utils/utils.py
+++ utils/utils.py
@@ -699,10 +699,10 @@ def calculate_mis(samples, y_true, alpha=0.1):
     samples = samples.reshape(samples.shape[0], -1)
     y_true = y_true.reshape(y_true.shape[0], -1)
 
-    # print(f"samples shape: {samples.shape}, y_true shape: {y_true.shape}")
+    print(f"samples shape: {samples.shape}, y_true shape: {y_true.shape}")
     lower = samples.quantile(alpha/2, dim=0)     # shape (T, D)
     upper = samples.quantile(1 - alpha/2, dim=0) # shape (T, D)
-    # print(f"lower shape: {lower.shape}, upper shape: {upper.shape}")
+    print(f"lower shape: {lower.shape}, upper shape: {upper.shape}")
     # Interval width component
     width = upper - lower
 
