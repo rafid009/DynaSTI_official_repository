48c8dc59a790c7b1fb269f5bb5e00f787205613e
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-11-21 17:51:08 -0800

    Topology ploting code added 1-1

---
+++ plot_topology.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+folder = "./topology_plots"
+if not os.path.isdir(folder):
+    os.makedirs(folder)
+train_locations_file = "./data/nacse/X_OR_temps_train_loc.npy"
+
+train_locations = np.load(train_locations_file)
+
+df_train = pd.DataFrame(train_locations, columns=['longitude', 'latitude', 'elevation'])
+df_train.to_csv(f"{folder}/nacse_train_locations.csv", index=False)
+
+test_locations_file = "./data/nacse/X_OR_temps_test_loc.npy"
+test_locations = np.load(test_locations_file)
+
+df_test = pd.DataFrame(test_locations, columns=['longitude', 'latitude', 'elevation'])
+df_test.to_csv(f"{folder}/nacse_test_locations.csv", index=False)
