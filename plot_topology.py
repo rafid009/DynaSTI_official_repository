2c8e4ce642d92541bbe8f3870b70c1cb2c650015
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-11-21 20:24:54 -0800

    Topology ploting code added 1-6

--- plot_topology.py
+++ plot_topology.py
@@ -10,11 +10,11 @@ train_locations_file = "./data/metr-la/X_train_locs.npy" #"./data/nacse/X_OR_tem
 
 train_locations = np.load(train_locations_file)
 
-df_train = pd.DataFrame(train_locations, columns=['longitude', 'latitude', 'elevation'])
+df_train = pd.DataFrame(train_locations, columns=['longitude', 'latitude'])
 df_train.to_csv(f"{folder}/metrla_train_locations.csv", index=False)
 
 test_locations_file = "./data/metr-la/X_test_locs.npy"
 test_locations = np.load(test_locations_file)
 
-df_test = pd.DataFrame(test_locations, columns=['longitude', 'latitude', 'elevation'])
+df_test = pd.DataFrame(test_locations, columns=['longitude', 'latitude'])
 df_test.to_csv(f"{folder}/metrla_test_locations.csv", index=False)
