cbc0b6836b83f1df2b5fe7c5dcab4bf4d95f62d3
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-11-21 19:53:20 -0800

    Topology ploting code added 1-4

--- plot_topology.py
+++ plot_topology.py
@@ -6,15 +6,15 @@ import os
 folder = "./topology_plots"
 if not os.path.isdir(folder):
     os.makedirs(folder)
-train_locations_file = "./data/awn/zone_8_train_locs.npy" #"./data/nacse/X_OR_temps_train_loc.npy"
+train_locations_file = "./data/metrla/X_train_locs.npy" #"./data/nacse/X_OR_temps_train_loc.npy"
 
 train_locations = np.load(train_locations_file)
 
 df_train = pd.DataFrame(train_locations, columns=['longitude', 'latitude', 'elevation'])
-df_train.to_csv(f"{folder}/nacse_train_locations.csv", index=False)
+df_train.to_csv(f"{folder}/metrla_train_locations.csv", index=False)
 
-test_locations_file = "./data/awn/zone_8_test_locs.npy"
+test_locations_file = "./data/metrla/X_test_locs.npy"
 test_locations = np.load(test_locations_file)
 
 df_test = pd.DataFrame(test_locations, columns=['longitude', 'latitude', 'elevation'])
-df_test.to_csv(f"{folder}/nacse_test_locations.csv", index=False)
+df_test.to_csv(f"{folder}/metrla_test_locations.csv", index=False)
