6415829b083657d4427199dda8c30c846a3a4f1c
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-11-25 08:45:52 -0800

    Checking uncertainty 1-18

--- plot_topology.py
+++ plot_topology.py
@@ -10,11 +10,11 @@ train_locations_file = "./data/nacse/X_OR_temps_train_loc.npy" # "./data/pems_ba
 
 train_locations = np.load(train_locations_file)
 
-df_train = pd.DataFrame(train_locations, columns=['longitude', 'latitude'])
+df_train = pd.DataFrame(train_locations, columns=['longitude', 'latitude', 'elevation'])
 df_train.to_csv(f"{folder}/nacse_train_locations.csv", index=False)
 
 test_locations_file = "./data/nacse/X_OR_temps_test_loc.npy"
 test_locations = np.load(test_locations_file)
 
-df_test = pd.DataFrame(test_locations, columns=['longitude', 'latitude'])
+df_test = pd.DataFrame(test_locations, columns=['longitude', 'latitude', 'elevation'])
 df_test.to_csv(f"{folder}/nacse_test_locations.csv", index=False)
