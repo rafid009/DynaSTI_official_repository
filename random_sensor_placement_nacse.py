4cd20258a4355f033894a81a72a4de5006be9815
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-11-25 13:06:14 -0800

    Sparse modified scenario added 1-2

--- random_sensor_placement_nacse.py
+++ random_sensor_placement_nacse.py
@@ -232,7 +232,7 @@ train_loader, test_loader = get_dataloader(total_stations, mean_std_file, n_feat
 lr = 0.01
 total_points = 50
 iters = 10 #00
-folder = 'results_map_random_sparse'
+folder = 'results_map_random_sparse_mod'
 if not os.path.isdir(folder):
     os.makedirs(folder)
 
