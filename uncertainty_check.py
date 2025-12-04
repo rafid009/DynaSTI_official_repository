8ae1f727b047f88ce58dd486c5bca8cb94c517a1
Mohammad Rafid Ul Islam <rai009@Mohammads-MacBook-Air.local>
2025-12-03 17:17:52 -0800

    Distance vs error relation test 1-9

--- uncertainty_check.py
+++ uncertainty_check.py
@@ -358,14 +358,14 @@ N = 4
 M = 1 # Number of virtual sensors to evaluate uncertainty on
 reference_coords = np.array([-123.28, 44.5]) # np.array([-121.10017, 45.20803]) # np.array([-121.68163, 45.34927])
 radius_m = 20000  # 20 km
-old = True
+old = False
 test_coords = sample_location(reference_coords[1], reference_coords[0], radius_m=radius_m, fix_lat=False, fix_lon=True, direction="south")
 test_coords = np.array([[test_coords[1], test_coords[0], 600.0]])  # Shape (1, 3)
 # test_coords = [[-123.3730556, 45.5447222, 991.0], [-123.5833333, 44.9166667, 1095.0], [-121.1333333, 45.45, 406.0], [-121.06508, 44.86472, 426.0]]
 
 print(f"Test coords: {test_coords}")
 # exclude_train_coords = [[-123.2966667, 45.5258333, 610.0], [-122.95, 45.5333333, 61.0], [-121.15596, 45.57307, 144.0], [-121.06748, 44.87365, 402.0], [-121.0683, 44.86912, 391.0]]
-train_loader, test_loader = get_dataloader(total_stations, mean_std_file, n_features, batch_size=1, missing_ratio=0.02, type=data_type, data=data, simple=simple, is_neighbor=is_neighbor, spatial_choice=spatial_choice, is_separate=is_separate, is_multi=is_multi, is_test=True, southeast=False, sparse=False, missing_dims=M, parts=False)#, test_loc=test_coords)
+train_loader, test_loader = get_dataloader(total_stations, mean_std_file, n_features, batch_size=1, missing_ratio=0.02, type=data_type, data=data, simple=simple, is_neighbor=is_neighbor, spatial_choice=spatial_choice, is_separate=is_separate, is_multi=is_multi, is_test=True, southeast=False, sparse=False, missing_dims=M, parts=False, test_loc=test_coords)
 
 model_diff_saits.eval()
 with torch.no_grad():
@@ -384,4 +384,4 @@ with torch.no_grad():
         uncertainty = compute_global_uncertainty_mean(samples)
 
         print(f"Uncertainty at test location {missing_locations}: {uncertainty.item()} and the inverse distance sum: {inverse_distance}\n")
-        # exit()
+        exit()
